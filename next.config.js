fccc451fdfefcaf1cff59501f810e0e1a2f3eca8
Daisuke <d.suke.kida@gmail.com>
2023-01-22 22:46:03 +0900

    🚑 deploy時の不具合修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  // via. https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/));
+    return config;
+  },
+};
