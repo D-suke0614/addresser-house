52d9ffc6d136f4812f82bec6afc9e687676716d0
Daisuke <d.suke.kida@gmail.com>
2023-02-02 22:10:13 +0900

    💚 build時に起きていたwebpackの問題を修正

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     // Important: return the modified config
-    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/));
+    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /\.stor(ies | y).[tj]sx$ / }));
     return config;
   },
 }
