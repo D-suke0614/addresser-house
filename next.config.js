3b3744cac0c81bca1d63d26c9979f41ac3036f9c
Daisuke <d.suke.kida@gmail.com>
2023-02-02 21:31:27 +0900

    ➕ storybookの導入

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/));
+    return config;
+  },
 }
 
 module.exports = nextConfig
