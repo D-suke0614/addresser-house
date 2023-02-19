5a9418a84e20dc5372e4ca69aa0059cb32719484
Daisuke <d.suke.kida@gmail.com>
2023-02-19 13:52:45 +0900

    🔥 不要なstorybookコンポーネントの削除

---
+++ src/stories/TextButton.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+// import { TextButton } from '@/components/ui/button/TextButton';
+import { TextButton } from '../components/ui/button/TextButton'
+
+export default {
+  title: 'Components/TextButton',
+  component: TextButton
+} as ComponentMeta<typeof TextButton>;
+
+const Template: ComponentStory<typeof TextButton> = (args) => <TextButton {...args} />
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'TextButton',
+  color: 'error',
+  size: 'small',
+};
